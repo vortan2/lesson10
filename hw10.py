19a3cf10a8464ed195648ce861ca4b5477ee4e6e
Vlad <vladislavk564@gmail.com>
2023-11-30 16:06:38 +0300

    создал репозиторий

---
+++ hw10.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+#1
+# file = open("example.txt").read()
+# print(len(file))
+#2
+# file = open("example.txt", "w")
+# text = input("Введите текст:")
+# file.write(text)
+#3
+ 
+# current_datetime = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+# print("Current date & time : ", current_datetime)
+ 
+# str_current_datetime = str(current_datetime)
+ 
+# file_name = str_current_datetime+".txt"
+# file = open(file_name, 'w')
+ 
+# print("File created : ", file.name)
+# file.close()
